d851c438c42427c27893c5d221a95187e71081b1
Anam0rph053 <n.coplo@gmail.com>
2019-01-09 16:33:17 +0100

    correctif avant merge

--- web/js/style.js
+++ web/js/style.js
@@ -2,11 +2,12 @@ function closeNav() {
     document.getElementById("myNav").style.width = "0%";
 }
 
+function openNav(){
+    document.getElementById("myNav").style.width = "100%";
+}
 
 $(function () {
 
-    $('#openNav').click(function () {
-        document.getElementById("myNav").style.width = "100%";
-    });
+    $('#openNav').click(openNav);
     $('#closeNav').click(closeNav);
 });
